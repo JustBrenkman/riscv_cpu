5e866a489b99e2ef4c18b6723c4961e0bc1c8c38
Ben Brenkman <JustBrenkman@gmail.com>
2020-02-21 10:12:25 -0700

    started working on wallace tre

---
+++ arithmetic/wallace_tree.sv
@@ -0,0 +1,13 @@
+module wallace_tree_32 #(
+    parameter WIDTH = 32;
+) 
+(
+    input wire logic[WIDTH - 1:0] a,
+    input wire logic[WIDTH - 1:0] b,
+    output logic;WIDTH - 1:0] result,
+    output logic overflow
+);
+
+
+
+endmodule
